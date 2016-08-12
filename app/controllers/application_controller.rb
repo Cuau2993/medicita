e8342dd1053982e72fec47f04f81393296da3c0b
cuau2993 <cuauhtemoc_2993@hotmail.com>
2016-08-11 21:55:58 -0500

    fixed root problem when logging in as a doctor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :validate_doctor
+
+  #def validate_doctor
+  #	if params[:controller]=='doctors'
+  #		unless current_user.isdoctor?
+  #			redirect_to patients_path
+  #		end
+  #	end
+  #end
+
 end
